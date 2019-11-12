d6d452dbfa9dcef1fd4da3e44cc86bb4bbef48da
Nikolui333 <semenovnikolu@yandex.ru>
2019-11-12 19:51:59 +0300

    Added database sqlite

--- app/src/main/java/com/example/posts/MainActivity.java
+++ app/src/main/java/com/example/posts/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.posts;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void buttonNew(View view) {
@@ -23,4 +31,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, postsList.class);
         startActivity(intent);
     }
-}
+    }
+
--- app/src/main/java/com/example/posts/NewEntry.java
+++ app/src/main/java/com/example/posts/NewEntry.java
@@ -1,14 +1,71 @@
 package com.example.posts;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class NewEntry extends AppCompatActivity {
+public class NewEntry extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnAdd, btnRead; //вторая кнопка, в дальнейшем чтение базы будет выполнятся из другой активности
+    EditText etText;
+
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_entry);
+
+        btnAdd = (Button) findViewById(R.id.buttonSave);
+        btnAdd.setOnClickListener(this);
+
+        btnRead = (Button) findViewById(R.id.btnRead);
+        btnRead.setOnClickListener(this);
+
+        etText = (EditText) findViewById(R.id.editText1);
+
+        dbHelper = new DBHelper(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String text = etText.getText().toString();
+
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        switch (v.getId()) {
+
+            case R.id.buttonSave:
+                contentValues.put(DBHelper.KEY_TEXT, text);
+
+                database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
+                break;
+
+            case R.id.btnRead:
+                Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
+
+                if (cursor.moveToFirst()) {
+                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_TEXT);
+                    do {
+                        Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
+                                ", name = " + cursor.getString(nameIndex));
+                    } while (cursor.moveToNext());
+                } else
+                    Log.d("mLog","0 rows");
+
+                cursor.close();
+                break;
+        }
+        dbHelper.close();
     }
 }
